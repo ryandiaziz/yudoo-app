0f17baf21be8d50fc710e1837140ca03151fb062
ryandiaziz <ryandiaziz.edu@gmail.com>
2024-01-23 13:54:44 +0700

    migrate to vite

--- src/components/elements/OutlineButton/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const OutlineButton = ({ name }: { name: String }) => {
-    return (
-        <button className='hover:bg-bg-200 btn-size border border-text-200 font-medium text-text-100'>{name}</button>
-    )
-}
-
-export default OutlineButton
--- src/components/fragments/ButtonContainerMenuBar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ButtonContainerMenuBar = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <div className='absolute bottom-16 w-full flex justify-evenly px-5'>{children}</div>
-    )
-}
-
-export default ButtonContainerMenuBar
