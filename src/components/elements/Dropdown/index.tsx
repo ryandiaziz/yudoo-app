2df9dc1269268592283543c5342355ae8302743a
Ryandi Aziz <ryandiaziz.edu@gmail.com>
2024-01-23 20:25:44 +0700

    feature -> category

--- src/components/elements/Dropdown/index.tsx
+++ src/components/elements/Dropdown/index.tsx
@@ -1,55 +1,25 @@
 import React from 'react'
 
+import DropdownItem from './components/dropdowItem'
+import DropdownContainer from './components/dropdownContainer'
+import Label from '../Label'
+
 const Dropdown = () => {
-    // const dropdownButton = document.getElementById('dropdown-button');
-    // const dropdownMenu = document.getElementById('dropdown-menu');
-    // const searchInput = document.getElementById('search-input');
-    // let isOpen = false; // Set to true to open the dropdown by default
-
-    // function toggleDropdown() {
-    //     open = !open;
-    //     Dropdown.classList.toggle('hidden', !open);
-    // }
-
-    // // Set initial state
-    // toggleDropdown();
-
-    // dropdownButton.addEventListener('click', () => {
-    //     toggleDropdown();
-    // });
-
-    // // Add event listener to filter items based on input
-    // searchInput.addEventListener('input', () => {
-    //     const searchTerm = searchInput.value.toLowerCase();
-    //     const items = Dropdown.querySelectorAll('a');
-
-    //     items.forEach((item) => {
-    //         const text = item.textContent.toLowerCase();
-    //         if (text.includes(searchTerm)) {
-    //             item.style.display = 'block';
-    //         } else {
-    //             item.style.display = 'none';
-    //         }
-    //     });
-    // });
     return (
-        <div className="min-h-screen flex items-center justify-center">
-            <div className="relative group">
-                <button id="dropdown-button" className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500">
-                    <span className="mr-2">Open Dropdown</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M6.293 9.293a1 1 0 011.414 0L10 11.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
-                    </svg>
-                </button>
-                <div id="dropdown-menu" className="hidden absolute right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1">
-                    <input id="search-input" className="block w-full px-4 py-2 text-gray-800 border rounded-md  border-gray-300 focus:outline-none" type="text" placeholder="Search items" autoComplete="off" />
-
-                    <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer rounded-md">Uppercase</a>
-                    <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer rounded-md">Lowercase</a>
-                    <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer rounded-md">Camel Case</a>
-                    <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer rounded-md">Kebab Case</a>
-                </div>
-            </div>
+        <div className='flex items-center'>
+            <Label name='Category' />
+            <DropdownContainer>
+                <DropdownItem
+                    title='Personal'
+                    value='personal'
+                    isSelected={false}
+                />
+                <DropdownItem
+                    title='Work'
+                    value='work'
+                    isSelected={false}
+                />
+            </DropdownContainer>
         </div>
     )
 }
